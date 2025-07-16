716ea644ab78c49dda5efd4d305cd1aa12cc5fe3
Nagham <Naghamkheir25@gmail.com>
2025-07-16 16:06:35 -0400

    Adding Firebase.

---
+++ frontend/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "meesa-6cc00.firebaseapp.com",
+  projectId: "meesa-6cc00",
+  storageBucket: "meesa-6cc00.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
