7c22ab94d2b66dc16a4af81c7f58e66fd114e446
Nagham <Naghamkheir25@gmail.com>
2025-07-19 03:55:20 -0400

    Deploying changes.

--- frontend/sw.js
+++ frontend/sw.js
@@ -11,7 +11,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request));
 });
 
-client = mqtt.connect("wss://241024dacc644661b296a5b1b68f1390.s1.eu.hivemq.cloud:8884/mqtt", {
-  username: "Meesa",
-  password: "MeesaPassword42"
-});
+// client = mqtt.connect("wss://241024dacc644661b296a5b1b68f1390.s1.eu.hivemq.cloud:8884/mqtt", {
+//   username: "Meesa",
+//   password: "MeesaPassword42"
+// });
